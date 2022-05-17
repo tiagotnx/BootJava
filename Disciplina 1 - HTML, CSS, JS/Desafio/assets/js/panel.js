ec08c348b5501e5347746a30d31a552b4c3460e1
TiagoTNX <tiagotnx@gmail.com>
2022-05-16 23:34:06 -0300

    modal interaction

--- Disciplina 1 - HTML, CSS, JS/Desafio/assets/js/panel.js
+++ Disciplina 1 - HTML, CSS, JS/Desafio/assets/js/panel.js
@@ -2,6 +2,7 @@ const menu = document.querySelectorAll("[data-action]");
 const formClient = document.querySelector("#client");
 const formProduct = document.querySelector("#product");
 const formOrder = document.querySelector("#order");
+const closeModal = document.querySelectorAll(".form__header > span");
 
 const getDataset = (action) => {
     switch (action) {
@@ -15,10 +16,15 @@ const getDataset = (action) => {
 };
 
 menu.forEach((item) => {
-    item.addEventListener("click", (e) => {
-        e.preventDefault();
+    item.addEventListener("click", () => {
         const action = item.dataset.action;
         const modal = getDataset(action);
         modal.classList.add("form-active");
+    });
+});
+
+closeModal.forEach((item) => {
+    item.addEventListener("click", () => {
+        item.parentElement.parentElement.classList.remove("form-active");
     });
 });
