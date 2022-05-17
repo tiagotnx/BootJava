60e0e9a6b7196e06cb0719c10b3b99cdb4c0e8f5
TiagoTNX <tiagotnx@gmail.com>
2022-05-17 15:13:11 -0300

    Close all modals

--- Disciplina 1 - HTML, CSS, JS/Desafio/assets/js/panel.js
+++ Disciplina 1 - HTML, CSS, JS/Desafio/assets/js/panel.js
@@ -2,8 +2,10 @@ const menu = document.querySelectorAll("[data-action]");
 const formClient = document.querySelector("#client");
 const formProduct = document.querySelector("#product");
 const formOrder = document.querySelector("#order");
-const closeModal = document.querySelectorAll(".form__header > span");
+const buttonCloseModal = document.querySelectorAll(".form__header > span");
+const allModals = document.querySelectorAll(".form");
 
+//modal interaction
 const getDataset = (action) => {
     switch (action) {
         case "client":
@@ -15,15 +17,22 @@ const getDataset = (action) => {
     }
 };
 
+const closeAllModals = () => {
+    allModals.forEach((item) => {
+        item.classList.remove("form-active");
+    });
+};
+
 menu.forEach((item) => {
     item.addEventListener("click", () => {
+        closeAllModals();
         const action = item.dataset.action;
         const modal = getDataset(action);
         modal.classList.add("form-active");
     });
 });
 
-closeModal.forEach((item) => {
+buttonCloseModal.forEach((item) => {
     item.addEventListener("click", () => {
         item.parentElement.parentElement.classList.remove("form-active");
     });
